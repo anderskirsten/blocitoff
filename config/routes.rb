909f5b2004d07d06acfc7c4732249a2e12e386d9
Kirsten Anderson <anders.kirsten@gmail.com>
2017-10-18 16:59:05 +0000

    User Profile (Basic): Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-
+  
   devise_for :users
   
+  resources :users, only: :show
+  
+  authenticated :user do
+    root 'users#show', as: :authenticated_root
+  end
+  
   get 'about' => 'welcome#about'
+  
+  get 'welcome' => 'welcome#index'
 
   root 'welcome#index'
 end
