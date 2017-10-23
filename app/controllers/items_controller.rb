4d7060086d8e4836c6120065100cbbfd16712c3a
Kirsten Anderson <anders.kirsten@gmail.com>
2017-10-23 17:47:05 +0000

    Mark Todos as Done/Deleted: Complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  respond_to :html, :js
+  
   def new
     @item = Item.new
   end
@@ -6,15 +8,28 @@ class ItemsController < ApplicationController
   def create
     @user = current_user
     
-    @item = Item.new(item_params, user_id: @user.id)
+    @item = Item.new(item_params)
     @item.user = @user
-      if @item.save!
-        flash[:notice] = "'#{@item.name}' has been added to your todo list."
-        redirect_to authenticated_root_path
-      else
-        flash[:alert] = "There was a problem saving this item to your todo list. Please try again."
-        redirect_to authenticated_root_path
-      end
+    
+    if @item.save!
+      flash[:notice] = "'#{@item.name}' has been added to your todo list."
+      redirect_to authenticated_root_path
+    else
+      flash[:alert] = "There was a problem saving this item to your todo list. Please try again."
+      redirect_to authenticated_root_path
+    end
+  end
+  
+  def destroy
+    @item = Item.find(params[:id])
+    
+    if @item.destroy
+      flash[:notice] = "'#{@item.name}' has been marked as complete and deleted from your todo list."
+      redirect_to authenticated_root_path
+    else
+      flash[:alert] = "There was a problem marking this item as complete. Please try again."
+      redirect_to authenticated_root_path
+    end
   end
   
   private
